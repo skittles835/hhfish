d742d8c9d43f55d919fb4cbb45c1477fba6eaebf
kelseacahill <kelseacahill@gmail.com>
2020-06-16 18:19:12 +0000

    fish fish fish

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-import pygame.examples.aliens as game
+import pygame
+from pygame.locals import *
+import sys
 
-game.main()
+pygame.init()
+screen_info = pygame.display.Info()
 
-print("AA FISH")
+screen_size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
+
+
+color = (0, 127, 255)
+
+def main():
+  while True:
+    clock.tick(60)
+    screen.fill(color)
+    pygame.display.flip()
+
+if __name__ == '__main__':
+  main()
